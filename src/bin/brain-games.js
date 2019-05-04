fca570f2f986caea5f5b2c2e86ba0ba25fb757a9
Nikita Borovkov <n.a.borovkov@mail.ru>
2019-05-04 14:38:11 +0300

    checkpoint 2 result

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,6 @@
 #!/usr/local/bin/node
 
+import name from '..';
 console.log('Welcome to the Brain Games!');
+const nameUser = name.question('May I have your name? ');
+console.log('Hello, ' + nameUser + '!');
